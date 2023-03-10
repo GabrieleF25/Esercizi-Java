acf007d3cfbe725c36d9cb0784023c22b568542e
GabrieleF25 <121727359+GabrieleF25@users.noreply.github.com>
2023-03-10 15:08:34 +0100

    Add files via upload

---
+++ Esercizio3/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Inserisci una stringa: ");
+        String input = sc.nextLine();
+
+        String output = invertString(input);
+
+        System.out.println("La stringa invertita è: " + output);
+    }
+
+    public static String invertString(String input) {
+        String output = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            output += input.charAt(i);
+        }
+        return output;
+    }
+}
